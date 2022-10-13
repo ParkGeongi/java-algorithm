de2909efd26ba92681a78fd336131505b8eb5133
ParkGeongi <pgg113579@naver.com>
2022-10-13 17:58:22 +0900

    2022/10/13

--- 정렬/삽입정렬.java
+++ 정렬/삽입정렬.java
@@ -1,11 +1,32 @@
 package 정렬;
 
-import java.util.Scanner;
-
-java.until.*;
-
 class 삽입정렬{
     public static void main(String[] args) {
+        new 삽입정렬().solution();       
+    }
+    
+    void solution(){
+        int[] arr = creatArray();
+        arr = sortArray(arr);
+        print(arr);
+    }
+    int creatrand(){
+        return (int)(Math.random()*10)+1;
+    }
+    int[] creatArray(){
+        int[] arr = new int[10];
+        for(int i=0;i<arr.length;i++){
+            arr[i] = creatrand();
+        }
+        return arr;
+    }
+    int[] sortArray(int[] arr){
+        return arr;
+    }
+    void print(int[] arr){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+"\t");
+        }
         
     }
 }
