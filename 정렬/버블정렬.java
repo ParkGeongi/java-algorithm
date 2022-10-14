52831ecce8679e027e39a4e9efc7a30a182d038b
ParkGeongi <pgg113579@naver.com>
2022-10-14 16:54:44 +0900

    2022-10-14

--- 정렬/버블정렬.java
+++ 정렬/버블정렬.java
@@ -1,55 +1,62 @@
 package 정렬;
-import java.util.*;
+
 class 버블정렬{
     public static void main(String[] args) {
         new 버블정렬().solution();
-        
     }
     void solution(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("숫자 개수: ");
+
+        int[] arr = creatArr();
+        arr = sortArr(arr);
+        print(arr);
+    }
+
+    int[] creatArr(){
         
-        int n = scanner.nextInt();
-        System.out.println("##########################");
-        int[] arr = creatArr(n);
-        System.out.println("##########################");
-        arr = sortArr(arr,n);
-        print(arr, n);
-    }
-    int creatRand(){
-        return (int)(Math.random()*10)+1;
-    }
-    int[] creatArr(int n){ 
-        int[] arr = new int[n];
-        for(int i=0; i< arr.length;i++){
-            int number = creatRand();
-            arr[i] =  number;
-            System.out.println(arr[i]);
+        int[] arr = new int[10];
+
+        for(int i=0; i< arr.length; i++){
+            arr[i] = randomNum();
+            for(int j=0; j<i; j++){
+                if(arr[j] == arr[i]){
+                    i--;
+                }  
+            }
         }
-               
+
         return arr;
     }
-    int[] sortArr(int[] arr,int n){
-        
-        for (int j=n-1; j>0;j--){
-            for (int i=0; i<j;i++){
-                if(arr[i]>arr[i+1]){
-                    int val = arr[i];
-                    arr[i] = arr[i+1];
-                    arr[i+1] = val;
-                    }
-                else {
-                    arr[i] = arr[i];
-                    arr[i+1] = arr[i+1];
+
+    int[] sortArr(int[] arr){
+
+        for(int i =0; i<arr.length; i++){
+            for(int j=0; j<arr.length - 1; j++){
+                if(arr[j]>arr[j+1]){
+                    int t = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = t;
+
                 }
+
+
             }
         }
         return arr;
     }
 
-    void print(int[] arr,int n){
-        for(int i=0; i<n; i++){
-            System.out.print(arr[i] +"\t");
+    int randomNum(){
+        return (int)(Math.random()*10)+1;
+    }
+
+    void print(int[] arr){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i] + "\t");
         }
     }
+
+
+
+
+
+
 }
--- 정렬/삽입정렬.java
+++ 정렬/삽입정렬.java
@@ -2,31 +2,49 @@ package 정렬;
 
 class 삽입정렬{
     public static void main(String[] args) {
-        new 삽입정렬().solution();       
+        new 삽입정렬().solution();
     }
-    
+
     void solution(){
-        int[] arr = creatArray();
-        arr = sortArray(arr);
+        int[] arr = creatArr();
+        print(arr);
+        System.out.println("\n");
+        arr = sortArr(arr);
         print(arr);
     }
-    int creatrand(){
-        return (int)(Math.random()*10)+1;
-    }
-    int[] creatArray(){
+
+    int[] creatArr(){
         int[] arr = new int[10];
         for(int i=0;i<arr.length;i++){
-            arr[i] = creatrand();
+            arr[i] = creatRandnum();
+            System.out.println("뽑은 랜덤값 :" + arr[i]);
         }
         return arr;
     }
-    int[] sortArray(int[] arr){
+
+    int creatRandnum(){
+        return (int)(Math.random()*10)+1;
+    }
+
+    int[] sortArr(int[] arr){
+        int val = 0;
+        for(int j=arr.length - 1;j>0;j--){
+            for(int i=0;i<j;i++){
+                if(arr[i] > arr[i+1]){
+                    val = arr[i];
+                    arr[i] = arr[i+1];
+                    arr[i+1] = val; 
+                }
+            }
+        }
         return arr;
     }
+
     void print(int[] arr){
         for(int i=0;i<arr.length;i++){
             System.out.print(arr[i]+"\t");
         }
-        
+       
     }
+
 }
